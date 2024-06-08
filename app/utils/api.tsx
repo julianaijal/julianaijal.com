84e9ac23c2a39cd5d471375ca09ddae88af1ab28
Julian Aijal <julian@thenextweb.com>
2024-06-08 17:23:29 +0200

    separate query string

--- app/utils/api.tsx
+++ app/utils/api.tsx
@@ -1,20 +1,27 @@
-const apiUrl = process.env.NEXT_PUBLIC_API_URL
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
 const fetchPosts = async () => {
+  const query = JSON.stringify({
+    query: `{
+      externalPostsPluralized {
+        title
+        url
+        symbol { url }
+      }
+    }`,
+  });
+
   try {
     const response = await fetch(`${apiUrl}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        query: `{
-          externalPostsPluralized {
-            title
-            url
-            symbol { url }
-          }
-        }`,
-      }),
+      body: query,
     });
-    if (!response.ok) throw new Error('Network response was not ok');
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+
     return await response.json();
   } catch (error) {
     console.error('Fetch error:', error);

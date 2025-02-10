2acdb1cb9ea3c416271a0fdbc3a8b03a0d19d019
Julian Aijal <julian.aijal@gmail.com>
2025-02-10 20:02:33 +0100

    format doc

--- app/_lib/SchemaArticle.tsx
+++ app/_lib/SchemaArticle.tsx
@@ -1,42 +1,43 @@
-import { ISchemaArticle } from '../_components/_interfaces/interfaces';
-
+import { ISchemaArticle } from "../_components/_interfaces/interfaces";
 
 const stripHtmlTags = (html?: string) => {
-   if (!html) return "";
-   return html
-   .replace(/<\/?[^>]+(>|$)/g, "")
-   .replace(/\\n|\\t|\\r/g, " ")
-   .replace(/\s+/g, " ")
-   .trim();
+  if (!html) return "";
+  return html
+    .replace(/<\/?[^>]+(>|$)/g, "")
+    .replace(/\\n|\\t|\\r/g, " ")
+    .replace(/\s+/g, " ")
+    .trim();
 };
 
 const decodeEscapedChars = (str: string) => {
-   return str.replace(/\\n/g, "\n").replace(/\\t/g, "\t").replace(/\\r/g, "");
- }
+  return str.replace(/\\n/g, "\n").replace(/\\t/g, "\t").replace(/\\r/g, "");
+};
 
-const SchemaArticle = ({ 
+const SchemaArticle = ({
   title,
   subtitle,
   content,
   headerImage,
   canonicalLink,
   createdBy,
-  slug
-}: ISchemaArticle ) => {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://julianaijal.com';
+  slug,
+}: ISchemaArticle) => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://julianaijal.com";
   const articleUrl = `${baseUrl}/articles/${slug}`;
 
-  const imageObject = headerImage ? {
-    "@type": "ImageObject",
-    "url": headerImage.url,
-    "width": headerImage.width || 1200,
-    "height": headerImage.height || 630
-  } : undefined;
+  const imageObject = headerImage
+    ? {
+        "@type": "ImageObject",
+        url: headerImage.url,
+        width: headerImage.width || 1200,
+        height: headerImage.height || 630,
+      }
+    : undefined;
 
   const authorObject = {
     "@type": "Person",
-    "name": createdBy?.name,
-    "url": baseUrl,
+    name: createdBy?.name,
+    url: baseUrl,
   };
 
   return (
@@ -47,24 +48,26 @@ const SchemaArticle = ({
           "@context": "https://schema.org",
           "@type": "Article",
           "@id": articleUrl,
-          "url": articleUrl,
-          "name": title,
-          "headline": title,
-          "description": subtitle,
-          "articleBody": content?.text ? decodeEscapedChars(stripHtmlTags(content.text)) : "",
-          "image": imageObject,
-          "author": authorObject,
-          "mainEntityOfPage": {
+          url: articleUrl,
+          name: title,
+          headline: title,
+          description: subtitle,
+          articleBody: content?.text
+            ? decodeEscapedChars(stripHtmlTags(content.text))
+            : "",
+          image: imageObject,
+          author: authorObject,
+          mainEntityOfPage: {
             "@type": "WebPage",
-            "@id": canonicalLink || articleUrl
+            "@id": canonicalLink || articleUrl,
           },
-          "publisher": {
+          publisher: {
             "@type": "Person",
-            "name": createdBy?.name,
-            "url": baseUrl
+            name: createdBy?.name,
+            url: baseUrl,
           },
-          "inLanguage": "en-US"
-        })
+          inLanguage: "en-US",
+        }),
       }}
     />
   );

a7129d55053a9de13fefb8bc6b537d7ef2bead4d
Julian Aijal <julian.aijal@gmail.com>
2025-02-14 14:28:48 +0100

    add origin isolation with COOP

--- next.config.js
+++ next.config.js
@@ -6,6 +6,30 @@ const nextConfig = {
       key: 'Strict-Transport-Security',
       value: 'max-age=63072000; includeSubDomains; preload',
     },
+    {
+      key: 'Cross-Origin-Opener-Policy',
+      value: 'same-origin'
+    },
+    {
+      key: 'Cross-Origin-Embedder-Policy',
+      value: 'require-corp'
+    },
+    {
+      key: 'X-Frame-Options',
+      value: 'SAMEORIGIN'
+    },
+    {
+      key: 'X-Content-Type-Options',
+      value: 'nosniff'
+    },
+    {
+      key: 'Referrer-Policy',
+      value: 'strict-origin-when-cross-origin'
+    },
+    {
+      key: 'Permissions-Policy',
+      value: 'camera=(), microphone=(), geolocation=()'
+    }
   ],
   sassOptions: {
     silenceDeprecations: ['legacy-js-api']

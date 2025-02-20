a3fea56f1f02eeeaf0b425445f6f9a63649ed806
Julian Aijal <julian.aijal@gmail.com>
2025-02-20 14:46:40 +0100

    comment out react-scan components and fix later

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Analytics } from "@vercel/analytics/react";
 import WebVitals from "./_lib/WebVitals";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Monitoring } from "react-scan/monitoring/next";
+// import { Monitoring } from "react-scan/monitoring/next";
 
 
 export const metadata: Metadata = {
@@ -17,19 +17,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  if (!process.env.REACT_SCAN_API_KEY) {
-    throw new Error('REACT_SCAN_API_KEY environment variable is not set');
-  }
+  // if (!process.env.REACT_SCAN_API_KEY) {
+  //   throw new Error('REACT_SCAN_API_KEY environment variable is not set');
+  // }
 
   return (
     <html lang="en">
       <GoogleTagManager gtmId="GTM-M8PS5F6" />
       <SpeedInsights/>
       <body>
-        <Monitoring
+        {/* <Monitoring
           apiKey={process.env.REACT_SCAN_API_KEY}
           url="https://monitoring.react-scan.com/api/v1/ingest"
-        />
+        /> */}
         <WebVitals />
         {children}
         <Analytics />

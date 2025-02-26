4b161bf187ede7d5ed4ff1042041975ad7652310
Julian Aijal <julian.aijal@gmail.com>
2025-02-26 22:21:46 +0100

    add dummy middleware

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+// https://vercel.com/docs/functions/edge-middleware/quickstart
+import { NextRequest, NextResponse } from 'next/server';
+import { geolocation } from '@vercel/functions';
+ 
+// The country to block from accessing the secret page
+const BLOCKED_COUNTRY = 'SE';
+ 
+// Trigger this middleware to run on the `/secret-page` route
+export const config = {
+  matcher: '/secret-page',
+};
+ 
+export default function middleware(request: NextRequest) {
+  // Extract country. Default to US if not found.
+  const { country = 'US' } = geolocation(request);
+ 
+  console.log(`Visitor from ${country}`);
+ 
+  // Specify the correct route based on the requests location
+  if (country === BLOCKED_COUNTRY) {
+    request.nextUrl.pathname = '/login';
+  } else {
+    request.nextUrl.pathname = `/secret-page`;
+  }
+ 
+  // Rewrite to URL
+  return NextResponse.rewrite(request.nextUrl);
+}

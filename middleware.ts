a3324151703897fe02597980819571c0f0962ed2
Julian Aijal <julian.aijal@gmail.com>
2025-02-26 22:25:33 +0100

    test page with blocked country

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { geolocation } from '@vercel/functions';
  
 // The country to block from accessing the secret page
-const BLOCKED_COUNTRY = 'SE';
+const BLOCKED_COUNTRY = 'US';
  
 // Trigger this middleware to run on the `/secret-page` route
 export const config = {
